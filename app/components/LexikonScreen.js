7fde8444ec02133ea77cff0c1053cbf237aa2707
Temeraire9 <katharina.garrecht@googlemail.com>
2017-11-29 17:10:45 +0100

    added coffee icons and scroll view for lexicon page

--- app/components/LexikonScreen.js
+++ app/components/LexikonScreen.js
@@ -1,16 +1,55 @@
 import React from 'react'
-import { StyleSheet, View, Button } from 'react-native'
+import {
+    StyleSheet,
+    View,
+    Button,
+    TouchableHighlight,
+    Image,
+    Text,
+    ScrollView
+} from 'react-native'
 import BottomNavigationContainer from './BottomNavigationContainer'
+import { coffees } from '../../app.json'
+import { colors, iconSizes, fontSizes } from '../styles'
 
 export default class LexikonScreen extends React.Component {
     render() {
         const { navigate } = this.props.navigation
+        coffeeImages = {
+            espresso: require('../assets/espresso.png'),
+            cappuccino: require('../assets/cappuccino.png'),
+            latteMacchiato: require('../assets/latteMacchiato.png'),
+            milchkaffee: require('../assets/milchkaffee.png'),
+            mokka: require('../assets/mokka.png'),
+            eiskaffee: require('../assets/eiskaffee.png'),
+            pharisaer: require('../assets/pharisaer.png'),
+            irishcoffee: require('../assets/irishcoffee.png')
+        }
         return (
             <View style={styles.view}>
-                <Button
-                    onPress={() => navigate('DetailLexikon')}
-                    title="DetailLexikon"
-                />
+                <ScrollView contentContainerStyle={styles.scrollView}>
+                    {coffees.map((coffee, index) => (
+                        <TouchableHighlight
+                            id={coffee.id}
+                            onPress={() => navigate('DetailLexikon')}
+                        >
+                            <View
+                                style={
+                                    index % 2 == 0
+                                        ? styles.outerReverse
+                                        : styles.outer
+                                }
+                            >
+                                <Image
+                                    style={styles.image}
+                                    source={coffeeImages[coffee.id]}
+                                />
+
+                                <Text style={styles.text}>{coffee.name}</Text>
+                            </View>
+                        </TouchableHighlight>
+                    ))}
+                </ScrollView>
                 <BottomNavigationContainer
                     navigation={this.props.navigation}
                     activeTab={2}
@@ -22,6 +61,33 @@ export default class LexikonScreen extends React.Component {
 
 const styles = StyleSheet.create({
     view: {
-        flex: 1
+        flex: 1,
+        justifyContent: 'space-between',
+        backgroundColor: colors.secondary,
+        paddingVertical: 20
+    },
+    scrollView: {
+        paddingBottom: 56
+    },
+    outer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        height: 120
+    },
+    outerReverse: {
+        flexDirection: 'row-reverse',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        height: 120
+    },
+    image: {
+        width: 80,
+        height: 80
+    },
+    text: {
+        color: colors.black,
+        fontSize: 18,
+        fontWeight: 'bold'
     }
 })
